1c1f8deead2bc0ce73c02557b9e6f73c0a450ff2
Hyejong Kim <hjkim@robotis.com>
2020-06-11 16:04:57 +0900

    add zsh theme

--- install_zsh.sh
+++ install_zsh.sh
@@ -21,7 +21,12 @@ curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | 
 echo "[Install Useful plugin]"
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions --depth=1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting --depth=1
-sed -i "s/plugins=(git)/plugins=(\ git\ zsh-autosuggestions\ zsh-syntax-highlighting\ )/" ~/.zshrc
+sed -i "s/plugins=(git)/plugins=(\\ git\\ zsh-autosuggestions\\ zsh-syntax-highlighting\\ )/" ~/.zshrc
+
+echo "[Install powerlevel10k theme]"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i "s/ZSH_THEME=\"/#ZSH_THEME=\"/" ~/.zshrc
+sed -i'' -r -e "/#ZSH_THEME/i\ZSH_THEME=\"powerlevel10k/powerlevel10k\"" ~/.zshrc
 
 urce $HOME/.zshrc
 
